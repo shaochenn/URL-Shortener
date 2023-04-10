a7c4561700b0112921725146edaf8c2973ada4bb
shaochenn <hnhngtb1111@gmail.com>
2023-04-10 17:28:07 +0800

    generate shortenURL and redirect shortURL

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars');
 const shortenURL = require('./shortenURL')
+const bodyParser = require('body-parser')
+const Link = require('./models/link')
+
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -11,10 +14,10 @@ const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-
 
 
 // 取得資料庫連線狀態
@@ -32,6 +35,30 @@ db.once('open', () => {
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const originURL = req.body.originURL
+  const shortURL = shortenURL()
+  if(!originURL){
+
+  } else {
+    Link.findOne({ originURL })
+      .lean()
+      .then(link => {
+        return link ? link : Link.create({originURL, shortURL})
+      })
+      .then(link => res.render('result', { origin: req.headers.origin, short: link.shortURL }))
+      .catch(error => console.error(error))
+  }
+})
+
+app.get('/:shortCode', (req, res) => {
+  const shortURL = req.params.shortCode
+
+  Link.findOne({ shortURL })
+    .then(link => res.redirect(link.originURL))
+    .catch(error => console.error(error))
 })
 
 // 設定 port 3000
--- models/link.js
+++ models/link.js
@@ -5,7 +5,7 @@ const linkSchema = new Schema({
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
   },
-  code: {
+  shortURL: {
     type: String,
     required: true
   }
